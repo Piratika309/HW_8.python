03c5eb7de6ad59ad2fa859914f37b76f37eb2378
Natali <piratika309@mail.ru>
2023-06-26 18:25:09 +0300

    тел справочник

---
+++ example01.py
@@ -0,0 +1,26 @@
+# Задача 38: Создать телефонный справочник возможностью добавления 
+# записей и чтения. Пользователь также может ввести фамилию, 
+# тогда программа должны вывести на экран все записи с этой фамилий. 
+# Также пользователь может добавлять новых людей в справочник в режиме экспорт.
+
+def imp():
+    surname = input (введите фамилию: )
+    name = input (введите имя: )
+    phone = input (введите телефонный номер: )
+
+    return surname, name, phone
+
+def controller():
+    node = int(input(Выберите режим работы со справочником: (1- найти контакт, 2 - создать контакт) ))
+    if node == 1:
+        pass
+    elif node == 2:
+        dateset = input.dateset()
+        write_person(dateset)
+    else:
+        print(' Такого режима не существует')
+        controller()
+def write_person(dateset: tuple):
+    with open('test.txt', 'a', encoding ='utf-8') as file:
+        file.write('\n')
+        file.write('\n'.join(dateset))
